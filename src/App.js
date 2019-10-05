e33f68c215b96e7850c147ffb573fbbb244f2337
Bin Mi <danielmees2014@gmail.com>
2019-10-05 21:12:20 +1000

    fetch data from json and unit test initial data fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './app.scss';
 
-function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: true,
+    resultsProperties: [],
+    savedProperties: [],
+    error: false
+  };
+
+  componentDidMount() {
+    return axios.get('http://localhost:3000/db/property.json')
+      .then(res => {
+        this.setState({
+          loading: false,
+          resultsProperties: res.data.results,
+          savedProperties: res.data.saved
+        });
+      })
+      .catch(() => {
+        this.setState({
+          loading: false,
+          error: 'Oops, something went wrong!',
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import axios from 'axios';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock('axios', () => {
+  const fakeResponse = {
+    data: {
+      results:['property1', 'property2'],
+      saved:[]
+    }
+  };
+  return {
+    get: jest.fn(() => Promise.resolve(fakeResponse)),
+  };
+});
+
+it('fetches properties results on #componentDidMount and update states correctly', (done) => {
+  const wrapper = shallow(<App />);
+  wrapper.instance()
+    .componentDidMount()
+    .then((response) => {
+      expect(axios.get).toHaveBeenCalled();
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/db/property.json');
+      expect(wrapper.state('loading')).toEqual(false);
+      expect(wrapper.state('resultsProperties')).toEqual(['property1', 'property2']);
+      expect(wrapper.state('savedProperties')).toEqual([]);
+      done();
+    });
 });
